6d4fc4d7e622ae171329012ec4ef5d47822e7cab
xintchen <xintchen@cisco.com>
2018-03-06 17:31:58 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
 const data = require('gulp-data');
 const watch = require('gulp-watch');
+const htmlBeautify = require('gulp-html-beautify');
 
 gulp.task('build', () => {
         gulp.src('src/**/*.html')
@@ -18,6 +19,18 @@ gulp.task('build', () => {
                 }]
             })))
             .pipe(nunjucks.compile())
+            .pipe(htmlBeautify({
+                indent_size: 4,
+                indent_char: ' ',
+                intent_with_tabs: true,
+                end_with_newline: true,
+                max_preserve_newlines: 1,
+                //brace_style: 'expand',
+                // 这里是关键，可以让一个标签独占一行
+                unformatted: true,
+                // 默认情况下，body | head 标签前会有一行空格
+                extra_liners: []
+            }))
             .pipe(gulp.dest('dist'))
     }
 )
