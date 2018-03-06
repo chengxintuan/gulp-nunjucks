331d4aec6f8653555b57c2e84a819cfe1accff59
xintchen <xintchen@cisco.com>
2018-03-06 15:24:51 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
 const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
 const data = require('gulp-data');
+const watch = require('gulp-watch');
 
-gulp.task('build', () =>
-    gulp.src('src/**/*.html')
-        .pipe(data(() => ({
-            name: 'home',
-            nav: [{
-                name: 'WebEx by Cisco'
-            }, {
-                name: 'Why WebEx?'
-            }, {
-                name: 'Products'
-            }, {
-                name: 'Plans & Pricing'
-            }]
-        })))
-        .pipe(nunjucks.compile())
-        .pipe(gulp.dest('dist'))
-);
+gulp.task('build', () => {
+        gulp.src('src/**/*.html')
+            .pipe(data(() => ({
+                name: 'home',
+                nav: [{
+                    name: 'WebEx by Cisco'
+                }, {
+                    name: 'Why WebEx?'
+                }, {
+                    name: 'Products'
+                }, {
+                    name: 'Plans & Pricing'
+                }]
+            })))
+            .pipe(nunjucks.compile())
+            .pipe(gulp.dest('dist'))
+    }
+)
+;
 
-// gulp.task('watch', () => {
-//     gulp.watch(['build'])
-// })
+gulp.task('watch', () => {
+    gulp.watch(['src/**/*'], ['build'])
+})
 
-gulp.task('default', ['build']);
+gulp.task('default', ['watch', 'build']);
